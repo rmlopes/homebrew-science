0ece29b4a4c1b33e76d0f7c208dbca49dbebe783
Rui Lopes <rui.lourenco.lopes@gmail.com>
2013-05-31 19:58:20 +0100

    added parsing of PYTHONPATH

--- pagmo.rb
+++ pagmo.rb
@@ -26,7 +26,8 @@ Is it linked against the correct python?"
     Dir.mkdir "build"
     Dir.chdir "build" do
       ppath = ENV['PYTHONPATH']
-      system "cmake",  "../", "-DBUILD_PYGMO=ON", "-DPYTHON_LIBRARY:FILEPATH=#{ppath}/libpython2.7.dylib", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+      ppath = ppath.split(":")[0]
+      system "cmake",  "../", "-DBUILD_EXAMPLES=ON", "-DBUILD_PYGMO=ON", "-DPYTHON_LIBRARY:FILEPATH=#{ppath}/libpython2.7.dylib", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
       system "make"
       system "make" ,"install" # if this fails, try separate make/make install steps
       system "ln", "-sf" , "#{lib}/python2.7/site-packages/PyGMO", "#{ppath}/python2.7/site-packages/"
